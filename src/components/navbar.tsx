41395eeb4bdd00f9b3a5e8a5fdba206a3cdbefc4
NuelOA <qwamehshel@gmail.com>
2022-12-09 16:30:52 +0000

    .

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link, NavLink } from "react-router-dom";
 import Logo from "../Assets/logo/bimaLogo.svg";
 
+
 export default function navbar() {
   return (
     
